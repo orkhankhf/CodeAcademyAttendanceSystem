412b8ee825843889ec3d0849e757e2e4202516e5
Orkhan Farajov <orkhankhf@code.edu.az>
2018-03-16 20:00:53 +0400

    Code Academy Attendance System Adminpanel - First Pushing

---
+++ CodeAcademyAttendanceSystem 16-03-2018 19-47/CodeAcademyAttendanceSystem/Controllers/HomeController.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Web;
+using System.Web.Mvc;
+using ZXing;
+using System.Linq;
+using ZXing.QrCode;
+using ZXing.QrCode.Internal;
+using CodeAcademyAttendanceSystem.Models;
+using System.Collections.Generic;
+
+namespace CodeAcademyAttendanceSystem.Controllers
+{
+    public class HomeController : Controller
+    {
+        CodeAcademyAttendanceSystem_dbEntities db = new CodeAcademyAttendanceSystem_dbEntities();
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Generate()
+        {
+            return View();
+        }
+        public ActionResult Generate_Qr_Code()
+        {
+            QrCodeEncodingOptions options = new QrCodeEncodingOptions();
+            Random rand = new Random();
+            string Qr_Code_Value = (rand.Next(100000000, 900000000)).ToString();
+            int group_id = 1;
+            DateTime today = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+
+            options = new QrCodeEncodingOptions
+            {
+                DisableECI = true,
+                CharacterSet = "UTF-8",
+                Width = 500,
+                Height = 500
+            };
+            var writer = new BarcodeWriter();
+            writer.Format = BarcodeFormat.QR_CODE;
+            writer.Options = options;
+            options.Hints.Add(EncodeHintType.ERROR_CORRECTION, ErrorCorrectionLevel.H);
+            var qr = new BarcodeWriter();
+            qr.Options = options;
+            qr.Format = BarcodeFormat.QR_CODE;
+            var result = new Bitmap(qr.Write(Qr_Code_Value));
+            string logo_img = Path.Combine(Server.MapPath("~/assets/img/"),"logo.png");
+            Image logo = Image.FromFile(logo_img);
+            int left = (result.Width / 2) - (logo.Width / 2);
+            int top = (result.Height / 2) - (logo.Height / 2);
+            Graphics g = Graphics.FromImage(result);
+            g.DrawImage(logo, new Point(left, top));
+            MemoryStream stream = new MemoryStream();
+            result.Save(stream, ImageFormat.Jpeg);
+            byte[] byteArray = stream.GetBuffer();
+            try
+            {
+                Qr_Codes qr_code = db.Qr_Codes.Where(q => q.qr_codes_date == today && q.qr_codes_group_id == group_id).Single();
+                qr_code.qr_codes_value = Qr_Code_Value;
+                qr_code.qr_codes_status = true;
+            }
+            catch
+            {
+                var Qr_Code = new Qr_Codes()
+                {
+                    qr_codes_date = today,
+                    qr_codes_status = true,
+                    qr_codes_value = Qr_Code_Value,
+                    qr_codes_group_id = 1
+                };
+                db.Qr_Codes.Add(Qr_Code);
+
+                List<int> students = db.Students.Where(s => s.student_group_id == group_id).Select(s => s.student_id).ToList();
+                foreach (var item in students)
+                {
+                    var new_attendance = new Students_Attendance()
+                    {
+                        students_attendance_date = today,
+                        students_attendance_sender_ip = null,
+                        students_attendance_status = false,
+                        students_attendance_student_id = item
+                    };
+                    db.Students_Attendance.Add(new_attendance);
+                }
+            }
+            db.SaveChanges();
+            return File(byteArray, "image/jpeg");
+        }
+    }
+}
---
+++ CodeAcademyAttendanceSystem 16-03-2018 19-47/CodeAcademyAttendanceSystem/Controllers/StudentController.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CodeAcademyAttendanceSystem.Models;
+using ZXing;
+
+namespace CodeAcademyAttendanceSystem.Controllers
+{
+    public class StudentController : Controller
+    {
+        CodeAcademyAttendanceSystem_dbEntities db = new CodeAcademyAttendanceSystem_dbEntities();
+
+        // GET: Student
+        public ActionResult Index()
+        {
+            Session.Remove("student_id");
+            Session.Remove("student_group_id");
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index([Bind(Include = "student_email, student_password")] Students student)
+        {
+            try
+            {
+                Students std = db.Students.Where(s => s.student_email == student.student_email && s.student_password == student.student_password).Single();
+                if (std != null)
+                {
+                    Session["student_id"] = std.student_id;
+                    Session["student_group_id"] = std.student_group_id;
+                    return RedirectToAction("ProfilePage");
+                }
+            }
+            catch
+            {
+                return View();
+            }
+            return View();
+        }
+
+        public ActionResult ProfilePage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Decode(HttpPostedFileBase qr_code)
+        {
+            var photo_name = Path.GetFileName(qr_code.FileName);
+            string photo_path = Path.Combine(Server.MapPath("/assets/img"), photo_name);
+            qr_code.SaveAs(photo_path);
+            IBarcodeReader reader = new BarcodeReader();
+            var barcodeBitmap = ConvertToBitmap(Server.MapPath("/assets/img/" + photo_name));
+            var result = reader.Decode(barcodeBitmap);
+            if (result != null)
+            {
+                var Qr_Code_Value = result.Text;
+                int group_id = Convert.ToInt32(HttpContext.Session["student_group_id"]);
+                int student_id = Convert.ToInt32(HttpContext.Session["student_id"]);
+                DateTime today = Convert.ToDateTime(DateTime.Now.ToShortDateString());
+                try
+                {
+                    Qr_Codes qr_codes_search = db.Qr_Codes.Where(q => q.qr_codes_group_id == group_id && q.qr_codes_date == today && q.qr_codes_value == Qr_Code_Value && q.qr_codes_status == true).Single();
+                    if(qr_codes_search != null)
+                    {
+                        var std_attendance = db.Students_Attendance.Where(a=>a.students_attendance_student_id == student_id && a.students_attendance_date == today).Single();
+                        std_attendance.students_attendance_sender_ip = "67.125.26.48";
+                        std_attendance.students_attendance_status = true;
+                        db.SaveChanges();
+                    }
+                    return RedirectToAction("ProfilePage", "Student");
+                }
+                catch
+                {
+                    return Content("Sizin grup ucun bele bir QR Code movcud deyil !");
+                }
+            }
+            return Content("QR Code oxunmadi !");
+        }
+
+
+        public Bitmap ConvertToBitmap(string fileName)
+        {
+            Bitmap bitmap;
+            using (Stream bmpStream = System.IO.File.Open(fileName, FileMode.Open))
+            {
+                Image image = Image.FromStream(bmpStream);
+
+                bitmap = new Bitmap(image);
+
+            }
+            return bitmap;
+        }
+    }
+}
